963f02de02b70473193cf6987083a2d8007b467a
Thiago Monteiro Nogueira <166952004+JamesMakarov@users.noreply.github.com>
2025-11-25 14:26:23 +0000

    Refactor derivative function and add approximation
    Refactor derivative calculation to improve efficiency and add a new approximated derivative function.

--- src/math_utils.cpp
+++ src/math_utils.cpp
@@ -10,30 +10,9 @@ double function_value(double a, double d) {
     return ((a * exp(d)) - (4 * pow(d, 2)));
 }
 
-/*
-
-that fun calculates the derivative, but it's using momory a lot, 'cause I was using vectors
-
-double derivate_function (double a, double d) {
-    double h = 0.1;
-    int i = 0;
-    vector<double> valores;
-
-    valores.push_back(d);
-    valores.push_back((function_value(a, d+h)-function_value(a,d))/h);
-
-    while (abs(valores[i]-valores[i+1]) >= 0.00001) {
-        i++;
-        h /= 10.0;
-        valores.push_back((function_value(a, d+h)-function_value(a,d))/h);
-    }
-
-    return valores[i+1];
-}
-*/
-
+// derivada que funciona pra qualquer função, porém, gasta mais poder computacional
 double derivate(double a, double d) {
-    
+    int cont = 0;
     double h = 0.1;
     double val_d1 = (function_value(a, d+h)-function_value(a,d-h))/(2*h);
 
@@ -51,8 +30,12 @@ double derivate(double a, double d) {
         val_d2 = (function_value(a, d+h)-function_value(a,d-h))/(2*h);
 
     }
-
+    cout << cont;
     return val_d2;
 }
 
-// and that calculates the normal function value
+// derivada aproximada, 
+double aproximated_derivative(double a, double d) {
+    double h = 0.000001;
+    return ((function_value(a, d+h)- function_value(a, d-h))/(2*h));
+}
