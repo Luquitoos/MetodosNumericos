636849bcd6ce01bb20d301e528d085a5dadfb223
Lucas Magalhães <lucasm.bizerril@gmail.com>
2025-11-24 21:45:03 -0300

    Melhoria Secante da função do luis

--- src/num_methods.cpp
+++ src/num_methods.cpp
@@ -31,30 +31,50 @@ double newton_modified_method(double a, double d) {
 
 */
 
-void secant_method(std::vector<double>* values_d,
-                   double a, double d1, double d2,
-                   int max_iter, const double tolerance)
-{
-    values_d->clear();
-    values_d->push_back(d1);
-    values_d->push_back(d2);
+void secant_method(std::vector<double>& values_d, double a, double d1, double d2, int max_iter, const double tolerance) {
+    
+    values_d.clear();
+    
+    double f1 = function_value(a, d1);
+    double f2 = function_value(a, d2);
+
+    if (std::abs(f1) < tolerance) {
+        values_d.push_back(d1);
+        return; 
+    }
+
+    values_d.push_back(d1);
+    values_d.push_back(d2);
+
+    if (std::abs(f2) < tolerance) {
+        return;
+    }
 
     for (int i = 0; i < max_iter; i++) {
-        double f1 = function_value(a, d1);
-        double f2 = function_value(a, d2);
-
+        
         if (std::abs(f2 - f1) < 1e-14) {
-            std::cerr << "Erro: divisão por zero iminente no método da secante.\n";
-            return;
+            std::cerr << "Erro devido a divisão por zero (f1 ~= f2) na iteração " << i << ".\n";
+            return; 
         }
 
-        double next = (d1*f2 - d2*f1) / (f2 - f1);
-        values_d->push_back(next);
+        double next = d2 - f2 * ((d2 - d1) / (f2 - f1));
+        
+        values_d.push_back(next);
 
-        if (std::abs(next - d2) < tolerance)
+        if (std::abs(next - d2) < tolerance) {
             return;
-
+        }
+        
         d1 = d2;
+        f1 = f2; 
+        
         d2 = next;
+        f2 = function_value(a, d2); 
+        
+        if (std::abs(f2) < tolerance) {
+            return;
+        }
     }
+    
+    std::cerr << "Convergência não alcançada após " << max_iter << " iterações.\n";
 }
