db3bff0ffea0aae92889e086d7d34aa521126a9b
Lucas Magalhães <lucasm.bizerril@gmail.com>
2025-11-25 15:12:31 -0300

    fix/type in .h

--- include/num_methods.h
+++ include/num_methods.h
@@ -2,8 +2,8 @@
 #define NUM_METHODS_H
 #include <vector> 
 
-double newton_raphson_method(std::vector<double>* values_d, double a, double d, int max, const double tolerance);
-double newton_modified_method(double a, double d); 
-double secant_method(std::vector<double>* values_d, double a, double d1, double d2, int max, const double tolerance);
+void newton_raphson_method(std::vector<double>* values_d, double a, double d, int max, const double tolerance);
+void newton_modified_method(double a, double d); 
+void secant_method(std::vector<double>* values_d, double a, double d1, double d2, int max, const double tolerance);
 
 #endif
